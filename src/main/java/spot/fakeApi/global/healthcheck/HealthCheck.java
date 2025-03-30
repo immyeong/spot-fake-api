7aefe08ec3413cc65efa4a51e259193673181cba
immyeong <wpfls6670@naver.com>
2025-03-30 15:46:15 +0900

    test: github actions

--- src/main/java/spot/fakeApi/global/healthcheck/HealthCheck.java
+++ src/main/java/spot/fakeApi/global/healthcheck/HealthCheck.java
@@ -11,6 +11,6 @@ public class HealthCheck {
     @GetMapping("/health-check")
     public String healthcheck() {
         log.info("health-check/ok");
-        return "ok!";
+        return "ok";
     }
 }
